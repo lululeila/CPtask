2acd4b3fd28cc857c6b0f3777bc47a4c55e66597
Absamtz <abuysalim93@gmail.com>
2025-03-28 23:45:49 +0300

    migrating from sqlite to postgres

--- CPtask/settings.py
+++ CPtask/settings.py
@@ -77,10 +77,16 @@ WSGI_APPLICATION = 'CPtask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cptask', 
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
